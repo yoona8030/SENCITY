0e212e62c2f0f5c160e1aa98e4510da6e7c96f51
yoona8030 <134251545+yoona8030@users.noreply.github.com>
2025-05-30 17:07:12 +0900

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {enableScreens} from 'react-native-screens';
+
+enableScreens();
+
+// screens 폴더에서 컴포넌트 임포트
+import Login from './src/screens/Login';
+import SignUp from './src/screens/SignUp';
+import Home from './src/screens/Home';
+
+export type RootStackParamList = {
+  Login: undefined;
+  SignUp: undefined;
+  Home: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
